e8f54ccaf9fa0d09cedf5a4cb02fed57cf85e5fa
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-09-08 19:11:14 -0700

    Add test fixture file for untested coverage tests

---
+++ test/fixtures/lib/multiply.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.multiply = function (a, b) {
+  return a * b;
+};
+
+exports.missed = function () {
+  return "This entire file is not covered because it's never required.";
+};
